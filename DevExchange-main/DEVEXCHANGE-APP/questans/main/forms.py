e10d9fac138a7ee87a8e865201f8586525a5941a
Hareeshma333 <hareeshmas40@gmail.com>
2024-04-03 16:38:43 +0530

    first commit

---
+++ DevExchange-main/DEVEXCHANGE-APP/questans/main/forms.py
@@ -0,0 +1,18 @@
+from django.forms import ModelForm
+from .models import Answer,Question
+from django.contrib.auth.models import User
+
+class AnswerForm(ModelForm):
+    class Meta:
+        model=Answer
+        fields=('detail',)
+
+class QuestionForm(ModelForm):
+    class Meta:
+        model=Question
+        fields=('title','detail','tags')
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model=User
+        fields=('first_name','last_name','username')
